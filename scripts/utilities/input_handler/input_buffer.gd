e974de0ab2d95f55768cc70fe0a5c5a802b6ca3d
richardmjaks <richardmiikael.jaks@gmail.com>
2024-10-22 06:39:37 +0300

    Fixed input buffering

--- scripts/utilities/input_handler/input_buffer.gd
+++ scripts/utilities/input_handler/input_buffer.gd
@@ -4,31 +4,32 @@ class_name InputBuffer
 ## Frames for how long to buffer input
 var buffer_time: int
 
-enum INPUT {ATTACK, DASH}
-
 var _buffer_queue_length: int = 0:
 	get():
-		return _buffer_input_queue.size()
-var _buffer_input_queue: Array[INPUT] = []
+		return _callback_function_queue.size()
 var _buffered_time_queue: Array[int] = []
-var _callback_function_queue: Array[Callable] = []
+var _callback_function_queue: Array[Callable] = [] 
 
 func _process(_delta: float) -> void:
+	var remove_indexes: Array[int] = []
 	for i in range(_buffer_queue_length):
 		var buffered_time: int = _buffered_time_queue[i] + 1
-		if buffered_time > buffered_time:
-			_unbuffer_input(i)
+		if buffered_time > buffer_time:
+			remove_indexes.append(i)
+			continue
 		_buffered_time_queue[i] = buffered_time
+	
+	# Unbuffer
+	for i in remove_indexes:
+		_unbuffer_input(i)
 
 func _unbuffer_input(idx: int) -> void:
-	_buffer_input_queue.remove_at(idx)
 	_buffered_time_queue.remove_at(idx)
 	_callback_function_queue.pop_at(idx).call()
 
 ## Buffers given input type, [param callback] is to unbuffer input once buffer runs out
-func buffer_input(type: INPUT, callback: Callable) -> void:	
+func buffer_input(callback: Callable) -> void:	
 	if _buffer_queue_length > 3:
 		return
-	_buffer_input_queue.append(type)
 	_buffered_time_queue.append(0)
 	_callback_function_queue.append(callback)
--- scripts/utilities/input_handler/player_input_handler.gd
+++ scripts/utilities/input_handler/player_input_handler.gd
@@ -4,10 +4,9 @@ class_name PlayerInputHandler
 @export var buffer_time = 5
 @onready var input_buffer: InputBuffer = $InputBuffer
 
-var wants_to_attack: bool = false:
-	set = _set_attack_intent
+var wants_to_attack: bool = false: set = _set_attack_intent
 
-var wants_to_dash: bool = false
+var wants_to_dash: bool = false: set = _set_dash_intent
 
 func _ready() -> void:
 	input_buffer.buffer_time = buffer_time
@@ -19,34 +18,41 @@ func _physics_process(delta: float) -> void:
 	).normalized()
 
 # We handle mouse input here so GUI elements could consume the input beforehand
+# Buffering is handled by setters of intents
 func _unhandled_input(event: InputEvent) -> void:
-	#TODO: We probably should handle input buffering here also
 	if event.is_action_pressed("a_attack"):
 		wants_to_attack = true
 	if event.is_action_pressed("a_dash"):
 		wants_to_dash = true
 
+# These setters also buffer the input
 #region Setters
-func _set_attack_intent(value: bool) -> bool:
+func _set_attack_intent(value) -> void:
 	# Avoid double-buffering
 	if not value:
-		return false
+		wants_to_attack = false
+		return
 	if wants_to_attack:
-		return true
+		return
 	
 	wants_to_attack = value
-	input_buffer.buffer_input(input_buffer.INPUT.ATTACK, _set_attack_intent.bind(false))
-	return value
+	input_buffer.buffer_input(
+		func(): wants_to_attack = false
+	)
+	wants_to_attack = true
 	
-func _set_dash_intent(value: bool) -> bool:
+func _set_dash_intent(value: bool) -> void:
 	# Avoid double-buffering
 	if not value:
-		return false
+		wants_to_dash = false
+		return
 	if wants_to_dash:
-		return true
+		return
 	
-	wants_to_attack = value
-	input_buffer.buffer_input(input_buffer.INPUT.DASH, _set_dash_intent.bind(false))
-	return value
+	wants_to_dash = value
+	input_buffer.buffer_input(
+		func(): wants_to_dash = false
+	)
+	wants_to_dash = true
 
 #endregion
